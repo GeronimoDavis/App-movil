9097bbb5c988555d00ac6324ee3e41a8af48961c
GeronimoDavis <geronimodavis@gmail.com>
2025-11-13 22:45:49 -0300

    first commit

---
+++ backend/ping-server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('OK desde e servidor'));
+app.listen(3000, '0.0.0.0', () => console.log('Servidor en puerto 3000'));
