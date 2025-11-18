7a80cc1214d349a72df3159305d3d45a8ecff126
GeronimoDavis <geronimodavis@gmail.com>
2025-11-18 19:25:48 -0300

    Preparando el servidor

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import habitRoutes from './routes/habitRoutes.js';
+
+dotenv.config();//cargar variables de entorno desde el archivo .env
+
+
+const app = express();//instancia de express
+
+app.use(cors({
+    origin: "http://localhost:5173",//frontend permitido
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    optionsSuccessStatus: 200
+}));//habilitar cors para permitir solicitudes desde otros dominios
+
+app.use(express.json());//para parsear el body de las solicitudes como json
+
+app.use('/api/habits', habitRoutes);//definimos la ruta base para las rutas de habitos
+
+const PORT = process.env.PORT || 5000;//puerto del servidor
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+});
