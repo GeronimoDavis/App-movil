36e11b7282c2192fe6bfb5947dd8a07be84c583d
GeronimoDavis <geronimodavis@gmail.com>
2025-11-26 21:34:37 -0300

    modelo de blacklist

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -123,3 +123,4 @@ export const refreshToken = async (req, res) =>{
         res.status(500).json({ message: "Server error", error: error.message });
     }
 }
+
---
+++ backend/models/blacklistedToken.js
@@ -0,0 +1,12 @@
+import mongoose, { Types } from "mongoose";
+
+const blacklistedTokenSchema = new mongoose.Schema({
+    token: {Types: String, required: true},
+    expiresAt: {type: Date, required: true}//guardamos la fecha de expiro del token para que cuando pase ese tiepo mongo lo borre
+})
+
+//creo un indice con todas las fechas y mongo elimina cada documento cuando su campo expiresAt < tiempo actual del servidor.
+blacklistedTokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+export default mongoose.model("BlacklistedToken", blacklistedTokenSchema);
+
