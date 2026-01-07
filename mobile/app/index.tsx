e96137905cba9592b53d4e779da8c0131edc0167
GeronimoDavis <geronimodavis@gmail.com>
2026-01-07 16:22:48 -0300

    Tomando el token y validando

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Redirect } from "expo-router";
+import { getToken } from "../src/storage/authStorage";
+
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>hello</Text>
-    </View>
-  );
+ const [isLogged, setIsLogged] = useState<boolean | null>(null); //toma valores bool o null y lo iniciamos como null
+
+ useEffect(() => {
+  const checkSession = async () =>{
+    const token = await getToken(); //usamos la funcion de authStorage para tomar el token y verificar que existe 
+    setIsLogged(!!token) //guarda true o false
+
+  };
+  checkSession();
+ },[]);
+
+ if(isLogged === null) return null;
+
+  if(!isLogged){
+    return <Redirect href="/login" />;
+  }
+  
+  
+  return <Redirect href="/home" />;
 }
